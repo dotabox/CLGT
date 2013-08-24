6323f3ffb5337225f4a6a0ec84380252b43ac1e5
thanhdeptrai <thanhpt.sami@gmai.com>
2013-08-24 10:45:33 +0700

    xoa tung doan 1.8

--- js/lang.js
+++ js/lang.js
@@ -1,16 +1,10 @@
 ﻿langvkl={
 	
 	sceneBattle:{
-		ID0:["Đợt quái: ","Wave: "],
-		ID1:["Ấn nút Start để bắt đầu.","Press Start to begin."],
-		ID2:[" giây."," seconds."],
-		ID3:["Đợt kế tiếp: ","Next wave: "]
+		ID0:["thành đẹp trai ","Wave: "],
+		ID1:["sdsd","Press Start to begin."],
+		ID2:["dsds"," seconds."],
+		ID3:["sdsd ","Next wave: "]
 	},
-	tut:{
-		ID0:["Nhấn vào đây","Click here"],
-		ID1:["Click để xây trụ","Click to build tower"],
-		ID2:["Nhấn vào để bắt đầu thủ thành","Click to start battle"],
-		ID3:["Vị trí quái ra","Monster starting point"],
-		ID4:["Vị trí đích đến của quái","Monster destination point"],
-	}
+	
 }
