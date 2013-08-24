73eaa7be91acab29f2f7d0bfc9a33c6a4dd49789
thanhdeptrai <thanhpt.sami@gmai.com>
2013-08-24 11:15:42 +0700

    xoa tung doan 1.31

--- js/lang.js
+++ js/lang.js
@@ -2,7 +2,7 @@ langvkl={
 	sceneBattle:{
 		ID0:["","Wave: "],
 		ID1:["","Press Start to begin."],
-		// để
+		//Tạo ra Monster khác để làm hình mờ đi
 		ID2:["giây."," seconds."],
 		ID3:["","Next wave: "]
 	},
