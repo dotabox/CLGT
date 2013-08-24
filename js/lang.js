c25f09f68b353380589fcae528fd697c48e9a390
thanhdeptrai <thanhpt.sami@gmai.com>
2013-08-24 10:33:02 +0700

    xoa tung doan 1.4

--- js/lang.js
+++ js/lang.js
@@ -1,3 +1,21 @@
 ﻿langvkl={
 	
+	sceneBattle:{
+		ID0:["Đợt quái: ","Wave: "],
+		ID1:["Ấn nút Start để bắt đầu.","Press Start to begin."],
+		ID2:[" giây."," seconds."],
+		ID3:["Đợt kế tiếp: ","Next wave: "]
+	},
+	tut:{
+		ID0:["Nhấn vào đây","Click here"],
+		ID1:["Click để xây trụ","Click to build tower"],
+		ID2:["Nhấn vào để bắt đầu thủ thành","Click to start battle"],
+		ID3:["Vị trí quái ra","Monster starting point"],
+		ID4:["Vị trí đích đến của quái","Monster destination point"],
+	},
+	guide:{
+		sell: ["Click vào trụ để bán", "Click on tower to sell"],
+		build: ["Chọn nơi để đặt trụ", "Choose area to place tower"],
+		life: [["Số mạng hiện tại: ","Current Life: "],["Số mạng ban đầu: ","Starting Life: "]]
+	}
 }
