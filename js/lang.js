dcfc4d1bb0f2214e8a627eb08d5dd3d20670712e
thanhdeptrai <thanhpt.sami@gmai.com>
2013-08-24 10:37:15 +0700

    xoa tung doan 1.7: xoa het tieng viet

--- js/lang.js
+++ js/lang.js
@@ -1,10 +1,10 @@
 ﻿langvkl={
 	
 	sceneBattle:{
-		ID0:["Đợt quái: ","Wave: "],
-		ID1:["Ấn nút Start để bắt đầu.","Press Start to begin."],
-		ID2:[" giây."," seconds."],
-		ID3:["Đợt kế tiếp: ","Next wave: "]
+		ID0:["sdd ","Wave: "],
+		ID1:["sdsd","Press Start to begin."],
+		ID2:["dsds"," seconds."],
+		ID3:["sdsd ","Next wave: "]
 	},
 	
 }
