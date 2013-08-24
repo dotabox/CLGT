bb1f0146675d274590ebb68a8cc7561dc57d2791
thanhdeptrai <thanhpt.sami@gmai.com>
2013-08-24 10:48:39 +0700

    xoa tung doan 1.9

--- js/lang.js
+++ js/lang.js
@@ -1,11 +1,7 @@
 ﻿langvkl={
-	
 	sceneBattle:{
-<<<<<<< HEAD
-		ID0:["thành đẹp trai ","Wave: "],
-=======
+		ID0:["tfdkjf","Wave: "],
 		ID0:["sdd ","Wave: "],
->>>>>>> dcfc4d1bb0f2214e8a627eb08d5dd3d20670712e
 		ID1:["sdsd","Press Start to begin."],
 		ID2:["dsds"," seconds."],
 		ID3:["sdsd ","Next wave: "]
