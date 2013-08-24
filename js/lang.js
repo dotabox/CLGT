82abe64a6990372f60ab462e72733f14b2a0a180
thanhdeptrai <thanhpt.sami@gmai.com>
2013-08-24 10:33:56 +0700

    xoa tung doan 1.5

--- js/lang.js
+++ js/lang.js
@@ -12,10 +12,5 @@
 		ID2:["Nhấn vào để bắt đầu thủ thành","Click to start battle"],
 		ID3:["Vị trí quái ra","Monster starting point"],
 		ID4:["Vị trí đích đến của quái","Monster destination point"],
-	},
-	guide:{
-		sell: ["Click vào trụ để bán", "Click on tower to sell"],
-		build: ["Chọn nơi để đặt trụ", "Choose area to place tower"],
-		life: [["Số mạng hiện tại: ","Current Life: "],["Số mạng ban đầu: ","Starting Life: "]]
 	}
 }
