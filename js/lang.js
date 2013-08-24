4551a65a7751165bfe4694eb3f624c22274425ab
thanhdeptrai <thanhpt.sami@gmai.com>
2013-08-24 10:54:27 +0700

    xoa tung doan 1.13

--- js/lang.js
+++ js/lang.js
@@ -3,6 +3,6 @@
 		ID0:["","Wave: "],
 		ID1:["Ấn nút Start để bắt đầu.","Press Start to begin."],
 		ID2:[" giây."," seconds."],
-		ID3:["Đợt kế tiếp: ","Next wave: "]
+		ID3:["","Next wave: "]
 	},
 }
