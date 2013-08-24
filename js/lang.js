c75bd8cb7cffe921e6975d3b63948322d8095713
thanhdeptrai <thanhpt.sami@gmai.com>
2013-08-24 10:32:08 +0700

    xoa tung doan 1.3

--- js/lang.js
+++ js/lang.js
@@ -1,34 +1,3 @@
 ﻿langvkl={
-	monster:{
-		Skill:{
-			ID0:["Tự hồi phục","Regeneration"],
-			ID1:["Trị liệu","Healing"],
-			ID2:["Né tránh","Evade"],
-			ID3:["Gia tốc","Haste"],
-			ID4:["Phòng ngự cực hạn","Harden"],
-			ID5:["Bất tử","Immunity"]
-		},
-		Textshow:{
-			ID2:["Né tránh","Miss"],
-			ID5:["Bất tử","Immunity"]
-		}
-	},
-	sceneBattle:{
-		ID0:["Đợt quái: ","Wave: "],
-		ID1:["Ấn nút Start để bắt đầu.","Press Start to begin."],
-		ID2:[" giây."," seconds."],
-		ID3:["Đợt kế tiếp: ","Next wave: "]
-	},
-	tut:{
-		ID0:["Nhấn vào đây","Click here"],
-		ID1:["Click để xây trụ","Click to build tower"],
-		ID2:["Nhấn vào để bắt đầu thủ thành","Click to start battle"],
-		ID3:["Vị trí quái ra","Monster starting point"],
-		ID4:["Vị trí đích đến của quái","Monster destination point"],
-	},
-	guide:{
-		sell: ["Click vào trụ để bán", "Click on tower to sell"],
-		build: ["Chọn nơi để đặt trụ", "Choose area to place tower"],
-		life: [["Số mạng hiện tại: ","Current Life: "],["Số mạng ban đầu: ","Starting Life: "]]
-	}
+	
 }
