89d7390c00de01b5efd3a3caf73384d400466873
thanhdeptrai <thanhpt.sami@gmai.com>
2013-08-24 11:12:30 +0700

    xoa tung doan 1.29

--- js/lang.js
+++ js/lang.js
@@ -1,7 +1,7 @@
 langvkl={
 	sceneBattle:{
 		ID0:["","Wave: "],
-		ID1:["đểu","Press Start to begin."],
+		ID1:["để","Press Start to begin."],
 		ID2:["giây."," seconds."],
 		ID3:["","Next wave: "]
 	},
