72aea2011fc585b4d28ea63e844bb087ee914a43
thanhdeptrai <thanhpt.sami@gmai.com>
2013-08-24 10:30:44 +0700

    xoa tung doan 1.2

--- js/lang.js
+++ js/lang.js
@@ -1,34 +1,4 @@
 ﻿langvkl={
-	
-	description:{
-		UserSkill:{
-			ID0:["Phát nổ trong một khoảng nhỏ, có khả năng gây bỏng.","Explosion thay may burn."],
-			ID1:["Tạo sấm sét giáng xuống trong một khoảng rộng, trừ giáp mục tiêu.","Lightning bolt that deal single-target damage and decrease its armor."],
-			ID2:["Đóng băng và gây sát thương trong một khoảng nhỏ.","Frozen enemies and deal damage in a small AoE."]
-		},
-		Tower:{
-			ID0:["Sát thương cao, tốc độ bắn nhanh với mục tiêu đơn lẻ.","High power single-target shot."],
-			ID1:["Có khả năng gây độc đối với mục tiêu.","Have chance to poison target."],
-			ID2:["Bắn lan với tốc độ bắn trung bình.","Medium attack speed but deal AoE damage."],
-			ID3:["Giảm tốc độ của mục tiêu.","Slow target's movement speed."],
-			ID4:["Tốc độ bắn nhanh và gây bỏng.","High-speed attack that burn enemies."],
-			ID5:["Sát thương cao, tốc độ bắn nhanh với mục tiêu đơn lẻ.","High power single-target shot."],
-			ID6:["Gia tăng sát thương khi máu của mục tiêu giảm.", "Amplify damage as enemies's hp drop."],
-			ID7:["Gây điện giật trong thời gian ngắn.", "Shock enemies in a short time."],
-			ID8:["Tăng tốc độ tấn công cho những trụ xung quanh.", "Boost attack speed for tower in range."],
-			ID9:["Khi trụ này kết liễu mục tiêu sẽ được thêm tiền thưởng.", "Bonus gold when last hit."],
-			ID10:["Có khả năng gây độc đối với mục tiêu.","Have chance to poison target."],
-			ID11:["Khi trụ này kết liễu một số lượng mục tiêu nhất định sẽ được thưởng thêm mạng.", "Bonus life when last hit."],
-			ID12:["Làm mục tiêu bị dính dầu trên người, gia tăng sát thương hệ hỏa đồng thời khiến quái trong phạm vi lây bỏng của nhau.", "Oil will amplify fire damage and make fire spread."],
-			ID13:["Gây bỏng đồng thời khiến mục tiêu phát nổ khi bị tiêu diệt.", "Burn and make enemies explode on dead."],
-			ID14:["Bắn lan với tốc độ bắn trung bình.","Medium attack speed but deal AoE damage."],
-			ID15:["Lan rộng độc tố.", "Spread the poison."],
-			ID16:["Gây bỏng đồng thời có khả năng bắn lan.", "AoE damage that burn."],
-			ID17:["Giảm tốc độ của mục tiêu.","Slow target's movement speed."],
-			ID18:["Đưa quái trong phạm vi vào trạng thái chống đẩy, bị chống đẩy từ nhiều trụ sẽ tăng sát thương lên nhiều lần.", "Push enemies, more push will multiply damage."],
-			ID19:["Tốc độ bắn nhanh và gây bỏng.","High-speed attack that burn enemies."],
-		},
-	},
 	monster:{
 		Skill:{
 			ID0:["Tự hồi phục","Regeneration"],
